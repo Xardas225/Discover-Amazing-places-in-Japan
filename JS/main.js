9ae0c22c2ad440ca45c4b7eeaf1cf12d2f0c2ac6
Alexandr <alexanderderevkov@yandex.ru>
2021-07-11 23:38:27 +0300

    added adaptive burger menu

--- JS/main.js
+++ JS/main.js
@@ -9,3 +9,15 @@
     };
 
 }());
+
+(function () {
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.header__nav')
+    const menuClose = document.querySelector('.header__nav-close')
+    burger.addEventListener('click', () => {
+        menu.classList.add('header__nav_active')
+    });
+    menuClose.addEventListener('click', () => {
+        menu.classList.remove('header__nav_active')
+    });
+}());
